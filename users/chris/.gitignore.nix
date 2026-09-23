286d6f0fa163984d583d2e376ab922f9b6721c27
Christopher Harrison <christopher.harrison@tweag.io>
2026-09-23 23:00:01 +0100

    gitignore my agent resume scripts

--- users/chris/.gitignore.nix
+++ users/chris/.gitignore.nix
@@ -1,11 +1,20 @@
-# Generate with:
-#
-# curl https://www.toptal.com/developers/gitignore/api/vim,terraform,rust,python,c,clojure \
-# | sed 's/ *#.*//;/^$/d;s/.*/"&"/' \
-# | sort \
-# | uniq
-
 [
+  ### Claude Code
+  "RESUME"
+  "resume-claude-*.sh"
+  ".claude"
+
+  ### Copilot
+  "resume-copilot-*.sh"
+]
+++ [
+  # Generate with:
+  #
+  # curl https://www.toptal.com/developers/gitignore/api/vim,terraform,rust,python,c,clojure \
+  # | sed 's/ *#.*//;/^$/d;s/.*/"&"/' \
+  # | sort \
+  # | uniq
+
   ### Clojure ###
   "pom.xml"
   "pom.xml.asc"
